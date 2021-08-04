29bf47630d4be58af80877821fb5ef155c1f4a8d
Johannes Konings <johannes.konings@outlook.com>
2021-08-04 23:20:49 +0200

    remove sk from dynamo

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -2,17 +2,12 @@ resource "aws_dynamodb_table" "aws_dynamodb_table" {
   name         = var.TABLE_NAME
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "pk"
-  range_key    = "sk"
 
   attribute {
     name = "pk"
     type = "S"
   }
 
-  attribute {
-    name = "sk"
-    type = "S"
-  }
 }
 
 resource "aws_kinesis_stream" "aws_kinesis_stream" {
