234550217b573f808618dd8c0b15d0abe4178ec3
Johannes Konings <johannes.konings@outlook.com>
2021-08-04 23:20:08 +0200

    update person loader lambda

--- terraform/src/persons-loader/index.js
+++ terraform/src/persons-loader/index.js
@@ -1,8 +1,11 @@
 const faker = require("faker");
+const AWS = require("aws-sdk");
 
 exports.handler = async (event) => {
   const batchSize = event.batchSize;
   let persons = [];
+  const tableName = process.env.TABLE_NAME;
+  const docClient = new AWS.DynamoDB.DocumentClient();
 
   for (let step = 0; step < batchSize; step++) {
     const person = {
@@ -14,6 +17,29 @@ exports.handler = async (event) => {
       jobArea: faker.name.jobArea(),
       jobType: faker.name.jobType(),
     };
+
+    const params = {
+      TableName: tableName,
+      Item: {
+        pk: String(Math.random()),
+        person: person,
+      },
+    };
+
+    await docClient
+      .put(params, function (err, data) {
+        if (err) {
+          const response = {
+            statusCode: 400,
+            body: err,
+          };
+          return response;
+        } else {
+          console.log("Added item:", JSON.stringify(data, null, 2));
+        }
+      })
+      .promise();
+
     persons = [...persons, person];
   }
 
